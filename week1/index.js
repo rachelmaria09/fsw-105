0cb8b0a45280b710e1609f33aa37e0c16b5788db
rachelmaria09 <rmgildea@gmail.com>
2020-06-06 11:51:31 -0400

    second

---
+++ week1/index.js
@@ -0,0 +1,26 @@
+//string
+var name = "Rachel";
+console.log(name);
+
+//number
+var age = 30;
+console.log(age);
+
+//boolean
+var becomingAWebDeveloper = true;
+console.log(becomingAWebDeveloper);
+
+//function
+var dog = {
+    firstName: "Waylon",
+    lastName: "Gildea",
+    breed: "chihuahua-mix",
+    dogAge: 8,
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+};
+console.log(dog);
+//groceryCart
+var shopper = ["apples", "bananas", "Clementines"];
+console.log(shopper);
